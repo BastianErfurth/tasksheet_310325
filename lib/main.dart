6e2bf265bc86b431149f085e10b2980fb41fe482
Bastian Erfurth <bastian.erfurth01@gmail.com>
2025-03-31 14:00:57 +0200

    more steps

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,15 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar.new(backgroundColor: Colors.blue),
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(backgroundColor: Colors.blue),
         body: Column(
           children: [
             SizedBox(height: 18),
-            Center(child: Text("Willkommen du Held")),
+            Center(child: Text("Willkommen bei Flutter")),
+            SizedBox(
+              height: 20,
+            ),
             Column(
               children: [
                 Container(
@@ -24,12 +28,63 @@ class MainApp extends StatelessWidget {
                   height: 80,
                   width: 190,
                 ),
+                SizedBox(
+                  height: 16,
+                ),
                 Container(
-                  child: Text("die Klammern machen mich verrückt"),
+                  child:
+                      Center(child: Text("die Klammern machen mich verrückt")),
                   color: Colors.yellow,
                   height: 80,
-                  width: 220,
-                )
+                  width: 280,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "So viele Möglichkeiten",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      " ich bin heute",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      " schon etwas überfordert",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 150,
+                ),
+                Container(
+                  child:
+                      Center(child: Text("Da heisst es: Übern, Üben,Üben...")),
+                  color: const Color.fromARGB(255, 188, 70, 125),
+                  height: 180,
+                  width: 290,
+                ),
               ],
             )
           ],
